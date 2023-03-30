35fa23663edbfbd2fc6cb9d06f364ffb9a725a3b
Évelyne <charlesdelorean1@icloud.com>
2023-03-30 13:57:26 -0400

    Initial commit

---
+++ OldABI.h
@@ -0,0 +1,14 @@
+//
+//  OldABI.h
+//  OldABI
+//
+//  Created by charl❤️‍🔥 on 2023-03-29.
+//
+
+#ifndef OldABI_h
+#define OldABI_h
+
+#include <stdio.h>
+#include "patchfind.h"
+
+#endif /* OldABI_h */
